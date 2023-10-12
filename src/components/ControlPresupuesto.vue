7020114d8ffb38a9d57ee0afef116d8bc194b2d1
Cristhian Sevilla <crissev17@gmail.com>
2023-10-12 14:44:18 -0600

    Panel de Presupuesto

---
+++ src/components/ControlPresupuesto.vue
@@ -0,0 +1,78 @@
+<script setup>
+import imagen from "../assets/img/grafico.jpg";
+</script>
+<template>
+  <div class="dos-columnas">
+    <div class="contenedor-grafico">
+      <img :src="imagen" />
+    </div>
+    <div class="contenedor-presupuesto">
+      <button class="reset-app">Resetear App</button>
+      <p>
+        <span>Presupuesto:</span>
+        $0
+      </p>
+      <p>
+        <span>Disponible:</span>
+        $0
+      </p>
+      <p>
+        <span>Gastado:</span>
+        $0
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.dos-columnas {
+  display: flex;
+  flex-direction: column;
+}
+.dos-columnas > :first-child {
+  margin-bottom: 4rem;
+}
+@media (min-width: 768px) {
+  .dos-columnas {
+    flex-direction: row;
+    gap: 4rem;
+    align-items: center;
+  }
+  .dos-columnas > :first-child {
+    margin-bottom: 0rem;
+  }
+}
+.reset-app {
+  background-color: #db2777;
+  border: none;
+  padding: 1rem;
+  width: 100%;
+  color: var(--blanco);
+  font-weight: 900;
+  text-transform: uppercase;
+  border-radius: 1rem;
+  transition-property: background-color;
+  transition-duration: 300ms;
+}
+.reset-app:hover {
+  background-color: #c21f68;
+  cursor: pointer;
+}
+.contenedor-presupuesto {
+  width: 100%;
+}
+.contenedor-presupuesto p {
+  font-size: 2.4rem;
+  text-align: center;
+  color: var(--gris-oscuro);
+}
+@media (min-width: 768px) {
+  .contenedor-presupuesto p {
+    text-align: left;
+  }
+}
+.contenedor-presupuesto span {
+  font-weight: 900;
+  color: var(--azul);
+}
+</style>
