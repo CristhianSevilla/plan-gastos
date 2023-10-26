faaea243dc375f545b07476786a3c3167b401ba9
Cristhian Sevilla <crissev17@gmail.com>
2023-10-26 15:50:26 -0600

    Mostrar gastos de la categoria seleccionada

--- src/components/Filtros.vue
+++ src/components/Filtros.vue
@@ -13,7 +13,7 @@ defineEmits(["update:filtro"]);
           :value="filtro"
           @input="$emit('update:filtro', $event.target.value)"
         >
-          <option value="">Selecciona una categoría</option>
+          <option value="">Todos los gastos</option>
           <option value="ahorro">Ahorro</option>
           <option value="comida">Comida</option>
           <option value="casa">Casa</option>
